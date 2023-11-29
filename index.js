ab0b3e97b7d91df9638ba5106bf0b4103bbb47d9
Altep <77551036+Altepp@users.noreply.github.com>
2023-11-29 10:14:35 +0100

    Update index.js
    Optimized, but still don't work

--- index.js
+++ index.js
@@ -1,171 +1,164 @@
 const gtfs = require("./gtfs.json");
 const download = require("file-download");
-let path = require('path')
-const agencies = gtfs.agencies;
-let unzipper = require ("unzipper")
-let {rm, access, createReadStream, createWriteStream, readdir, readFile, writeFile } = require("fs");
-let ref = require("./ref.json");
-let fs = require("fs").promises
-
-let UniJSONPath = path.join(__dirname, "..", "UniJSON")
-let tempDir = path.join(__dirname, "temp")
+const unzipper = require("unzipper");
+const { rm, access, readdir, readFile, writeFile } = require("fs").promises;
+const { createReadStream, createWriteStream } = require("fs");
+const path = require('path');
+const ref = require("./ref.json"); // Ajoute cette ligne pour définir ref
+const { removeInvisibleCharacters } = require('remove-invisible-characters');
 
 
-access(tempDir, function(error) {
-    if (!error) {
-        rm(tempDir, { recursive: true }, err => {
-            if (err) {
-            throw err
-            }
-        
-            console.log(`Deleted temp folder from incompleted unijson convertion !`)
-        })
+let linesCount = 0
+const UniJSONPath = path.join(__dirname, "..", "UniJSON");
+const tempDir = path.join(__dirname, "temp");
+
+async function main() {
+    try {
+        await cleanupTempDir();
+        await downloadAndProcessFiles();
+    } catch (error) {
+        console.error("An error occurred:", error);
     }
-})
-
-
-// Parcourir les agences
-agencies.forEach((agency) => {
-
-    i = 0 
-    agency.urls.forEach(url => {
-        i++
-        downloadFile(url, i, agency.name)
-    })
-
-});
-
-
-
-
-
-
-
-
-
-async function downloadFile (url, index, name) {
-
-
-
-    //Wrapping the code with an async function, just for the sake of example.
-    console.log("Dowloading: " + url)
-    let tempDirOutput = path.join(tempDir, name)
-    let filename = name + i + ".zip" 
-    var options = {
-        directory: tempDirOutput,
-        filename: filename
-    }
-
-    download(url, options, function(err){
-        if (err) return console.log("Cannot download:" + filename)
-        console.log("Downloaded succesfully:", filename)
-        unzipAndUniJSONify(tempDirOutput, filename, UniJSONPath)
-    })
-
-
 }
+
+async function cleanupTempDir() {
+    try {
+        await rm(tempDir, { recursive: true });
+        console.log("Deleted temp folder from incomplete unijson conversion!");
+    } catch (error) {
+        console.error("Error cleaning up temp folder:", error);
+    }
+}
+
+async function downloadAndProcessFiles() {
+    const agencies = gtfs.agencies;
+    
+    for (const agency of agencies) {
+        let i = 0;
+
+        for (const url of agency.urls) {
+            i++;
+            await downloadFile(url, i, agency.name);
+        }
+    }
+}
+
+async function downloadFile(url, index, name) {
+    try {
+        console.log("Downloading: " + url);
+        const tempDirOutput = path.join(tempDir, name);
+        const filename = name + index + ".zip";
+        const options = { directory: tempDirOutput, filename: filename };
+
+        await downloadFileAsync(url, options);
+        await unzipAndUniJSONify(tempDirOutput, filename, UniJSONPath, ref);
+    } catch (error) {
+        console.error("Error downloading file:", error);
+    }
+}
+
+
+async function downloadFileAsync(url, options) {
+    return new Promise((resolve, reject) => {
+        download(url, options, (err) => {
+            if (err) {
+                reject(`Cannot download: ${options.filename}`);
+            } else {
+                console.log(`Downloaded successfully: ${options.filename}`);
+                resolve();
+            }
+        });
+    });
+}
+
+// ... (le reste de votre code)
 
 async function unzipAndUniJSONify(dir, filename, output) {
-    let fileJSON = {}
+    try {
+        const fileJSON = {};
+        const folder = path.join(dir, filename.split(".")[0]);
+        const filenameShort = filename.split(".")[0];
 
-    let folder = path.join(dir, filename.split(".")[0])
-    let filenameShort = filename.split(".")[0]
+        await createReadStream(path.join(dir, filename))
+            .pipe(unzipper.Extract({ path: folder }))
+            .on('entry', entry => entry.autodrain())
+            .promise();
 
-    createReadStream(path.join(dir, filename))
-        .pipe(unzipper.Extract({ path: folder }))
-        .on('entry', entry => entry.autodrain())
-        .promise()
-        .then(async () => {
+        console.log('Extracted: ' + filename);
 
-            console.log('Extracted: ' + filename)
+        await rm(path.join(dir, filename));
 
-            rm(path.join(dir, filename), err => {
+        const files = await readdir(folder);
 
-                if (err) {
-                    console.log("Can't delete: " + filename)
-                } else {
-                    console.log("Deleted: " + filename)
-                }
+        for (const file of files) {
+            const filePath = path.join(folder, file);
+            const fileType = file.split(".")[0];
 
-            })
+            const fileContent = await readFile(filePath, 'utf-8');
+
+            console.log("Found " + fileType + " for " + filenameShort);
+
+            await treatFile(fileContent, ref[fileType], fileType, path.join(output, filenameShort + ".json"), fileJSON);
+        }
+        console.log(linesCount, "lines of data treated")
+    } catch (error) {
+        console.error("Error in unzipAndUniJSONify:", error);
+    }
+}
+
+async function treatFile(content, ref, type, out, fileJSON) {
+    try {
+        let fileLineTemp = content.split("\r\n");
+
+        console.log("Found", fileLineTemp.length, "lines in", type);
+        linesCount= linesCount+fileLineTemp.length
+        let fileItem = [];
+        fileLineTemp.forEach(line => {
+            fileItem.push(line.split(","));
+        });
 
 
-            readdir(folder, async (err, files) => {
-                if (err) {
-                    console.log("Cannot get", filenameShort, "gtfs files\n", err)
-                } else {
-                    try {
-                        const files = await fs.readdir(folder);
-                
-                        for (const file of files) {
-                            const filePath = path.join(folder, file);
-                            const fileType = file.split(".")[0];
-                
-                            try {
-                                const fileContent = await fs.readFile(filePath, 'utf-8');
+        fileItem[0].forEach(item => {
+            let ref2;
+            ref.forEach(refer => ref2 = refer)
+    
+            let hasRef = false 
+            if(ref2 = item){
+                hasRef = true
+            }
+    
+            if(!hasRef) {
+                fileItem.unshift(ref)
+            }
+    
+    
+    
+        })
 
-                                console.log("Found " + fileType + " for " + filenameShort);
-                                
-                                await treatFile(fileContent, ref[fileType], fileType, path.join(output, filenameShort + ".json"), fileJSON);
-                            } catch (err) {
-                                console.log("Cannot read", filePath, err);
-                            }
-                        }
-                    } catch (err) {
-                        console.error("Error reading folder:", err);
-                    }
-                }
-            })
-        },
-        e => console.log('error',e));
+        let temp = [];
 
+        for (let i = 1; i < fileItem.length - 0; i++) {
+            let temp2 = {};
+
+            for (let j = 0; j < fileItem[0].length - 0; j++) {
+                temp2[fileItem[0][j].replace(/[]/g, "")] = fileItem[i][j]
+            }
+
+            temp.push(temp2);
+        }
+
+        if (!fileJSON[type]) {
+            fileJSON[type] = [];
+        }
+
+        fileJSON[type] = fileJSON[type].concat(temp);
+
+        await writeFile(out, JSON.stringify(fileJSON), 'utf-8');
+    } catch (err) {
+        console.log("Error in treatFile:", err);
+    }
 }
 
 
-function treatFile(content, ref, type, out, fileJSON) {
-    let fileLineTemp = content.split("\r\n")
 
-    console.log("Found", fileLineTemp.length, "lines in", type)
-
-    let fileItem = []
-
-    fileLineTemp.forEach(line => {
-        fileItem.push(line.split(","))
-    });
-    
-    fileItem[0].forEach(item => {
-        let ref2;
-        ref.forEach(refer => ref2 = refer)
-
-        let hasRef = false 
-        if(ref2 = item){
-            hasRef = true
-        }
-
-        if(!hasRef) {
-            fileItem.unshift(ref)
-        }
-    })
-
-    let temp = []
-    for (let i = 1; i < fileItem.length; i++) {
-    
-        let temp2 = {}
-
-        for (let j = 0; j < fileItem[0].length; j++) {
-           temp2[fileItem[0][j]] = fileItem[i][j]
-           temp.push(temp2)
-        }
-        //console.log(temp)
-        fileJSON[type] = temp
-
-
-    }
-    writeFile(out, JSON.stringify(fileJSON), 'utf-8', (err) => {
-        if(err) {
-            console.log("Cannot write JSON", err)
-        }
-    }
-    )
-}
-
+main();  // Appelle la fonction principale pour démarrer le processus
